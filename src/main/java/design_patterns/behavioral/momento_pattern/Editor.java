439942c520d625d51b513a263d02a149a17c55af
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-07 13:37:05 +0330

    Implementing Editor class createState and restore methods

--- src/main/java/design_patterns/behavioral/momento_pattern/Editor.java
+++ src/main/java/design_patterns/behavioral/momento_pattern/Editor.java
@@ -7,4 +7,16 @@ import lombok.Setter;
 @Setter
 public class Editor {
     private String content;
+
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state) {
+        try {
+            this.content = state.content();
+        } catch (NullPointerException e) {
+            this.content = "";
+        }
+    }
 }
