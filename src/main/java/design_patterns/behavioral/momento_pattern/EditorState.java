3d6c5a512a3e1aeba2c6e681624287367271aa20
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-07 13:36:06 +0330

    Converting EditorState class to record

--- src/main/java/design_patterns/behavioral/momento_pattern/EditorState.java
+++ src/main/java/design_patterns/behavioral/momento_pattern/EditorState.java
@@ -1,10 +1,4 @@
 package design_patterns.behavioral.momento_pattern;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class EditorState {
-    private final String content;
+public record EditorState(String content) {
 }
