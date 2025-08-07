6406cc0c62c3c8f5b5d441f99c77099af335906c
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-07 13:36:26 +0330

    Implementing History class

--- src/main/java/design_patterns/behavioral/momento_pattern/History.java
+++ src/main/java/design_patterns/behavioral/momento_pattern/History.java
@@ -1,4 +1,19 @@
 package design_patterns.behavioral.momento_pattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class History {
+    private final List<EditorState> states = new ArrayList<>();
+
+    public void push(EditorState state) {
+        states.add(state);
+    }
+
+    public EditorState pop() {
+        if (states.isEmpty()) {
+            return null;
+        }
+        return states.removeLast();
+    }
 }
