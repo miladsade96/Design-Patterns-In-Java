64d0e7775791b8f37d1f40f5fd9ebc94eb182b31
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-13 20:40:20 +0330

    Implementing canvas class methods without using state pattern

--- src/main/java/design_patterns/behavioral/state_pattern/Canvas.java
+++ src/main/java/design_patterns/behavioral/state_pattern/Canvas.java
@@ -1,4 +1,23 @@
 package design_patterns.behavioral.state_pattern;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Canvas {
+
+    private ToolType currentTool;
+
+    public void mouseUp() {
+        if (currentTool == ToolType.SELECTION) System.out.println("Selection Icon");
+        else if (currentTool == ToolType.BRUSH) System.out.println("Brush Icon");
+        else if (currentTool == ToolType.ERASER) System.out.println("Eraser Icon");
+    }
+
+    public void mouseDown() {
+        if (currentTool == ToolType.SELECTION) System.out.println("Draw Dashed Rectangle");
+        else if (currentTool == ToolType.BRUSH) System.out.println("Draw A Line");
+        else if (currentTool == ToolType.ERASER) System.out.println("Erase Something");
+    }
 }
