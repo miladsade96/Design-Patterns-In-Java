21545b98bdebd5412b630bda7a343180030404db
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-15 22:26:27 +0330

    Implementing the methods of BrowseHistory class

--- src/main/java/design_patterns/behavioral/iterator_pattern/BrowseHistory.java
+++ src/main/java/design_patterns/behavioral/iterator_pattern/BrowseHistory.java
@@ -1,4 +1,19 @@
 package design_patterns.behavioral.iterator_pattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowseHistory {
+    private final List<String> urls = new ArrayList<>();
+
+    public void push(String url) {
+        urls.add(url);
+    }
+
+    public String pop() {
+        int lastIndex = urls.size() - 1;
+        String lastUrl = urls.get(lastIndex);
+        urls.remove(lastUrl);
+        return lastUrl;
+    }
 }
