f6017e3179e6498a390f82074835713d3bf5e648
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-15 22:56:03 +0330

    Implementing BrowseHistory class using iterator pattern

--- src/main/java/design_patterns/behavioral/iterator_pattern/BrowseHistory.java
+++ src/main/java/design_patterns/behavioral/iterator_pattern/BrowseHistory.java
@@ -1,13 +1,15 @@
 package design_patterns.behavioral.iterator_pattern;
 
-import lombok.Getter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
+
 public class BrowseHistory {
     private final List<String> urls = new ArrayList<>();
+
+    public Iterator<String> createIterator() {
+        return new ListIterator(this);
+    }
 
     public void push(String url) {
         urls.add(url);
@@ -18,5 +20,29 @@ public class BrowseHistory {
         String lastUrl = urls.get(lastIndex);
         urls.remove(lastUrl);
         return lastUrl;
+    }
+
+    public static class ListIterator implements Iterator<String> {
+        private final BrowseHistory history;
+        private int index;
+
+        public ListIterator(BrowseHistory history) {
+            this.history = history;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < history.urls.size());
+        }
+
+        @Override
+        public String current() {
+            return history.urls.get(index);
+        }
+
+        @Override
+        public void next() {
+            index++;
+        }
     }
 }
