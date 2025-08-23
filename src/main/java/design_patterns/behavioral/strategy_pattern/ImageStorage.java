a3cbbb166e9839187c0f9762b4eecc19d587bfc2
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-08-23 22:21:31 +0330

    Implementing ImageStorage class using strategy pattern

--- src/main/java/design_patterns/behavioral/strategy_pattern/ImageStorage.java
+++ src/main/java/design_patterns/behavioral/strategy_pattern/ImageStorage.java
@@ -1,17 +1,8 @@
 package design_patterns.behavioral.strategy_pattern;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class ImageStorage {
-    private String compressor;
-    private String filter;
-
-    private void store(String fileName) {
-        if (compressor.equals("jpeg")) System.out.println("Compressing using jpeg");
-        else if (compressor.equals("png")) System.out.println("Compressing using png");
-
-        if (filter.equals("b&w")) System.out.println("Applying b&w filter");
-        if (filter.equals("high-contrast")) System.out.println("Applying high-contrast filter");
+    public void store(String fileName, Compressor compressor, Filter filter) {
+        compressor.compress(fileName);
+        filter.apply(fileName);
     }
 }
