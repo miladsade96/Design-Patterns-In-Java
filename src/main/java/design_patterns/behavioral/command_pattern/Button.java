4f6d93693a2b5b3d6a5726ed8fc7fbf06acedba2
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-08-29 20:59:12 +0330

    Implementing Button class

--- src/main/java/design_patterns/behavioral/command_pattern/Button.java
+++ src/main/java/design_patterns/behavioral/command_pattern/Button.java
@@ -1,4 +1,14 @@
 package design_patterns.behavioral.command_pattern;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Button {
+    private String label;
+
+    private void click() {
+        // ............
+    }
 }
