fa51acc9876d2394fde3bb04e86796fc4831cb83
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-02 17:08:43 +0330

    Creating necessary classes for mediator pattern

---
+++ src/main/java/design_patterns/behavioral/mediator/ListBox.java
@@ -0,0 +1,13 @@
+package design_patterns.behavioral.mediator;
+
+public class ListBox extends UIControl {
+    private String selection;
+
+    public String getSelection() {
+        return selection;
+    }
+
+    public void setSelection(String selection) {
+        this.selection = selection;
+    }
+}
---
+++ src/main/java/design_patterns/behavioral/mediator/UIControl.java
@@ -0,0 +1,4 @@
+package design_patterns.behavioral.mediator;
+
+public class UIControl {
+}
