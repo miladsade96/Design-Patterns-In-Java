ea4f8a813cedc937d42833aa889fa311b997ad01
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-04 13:07:17 +0330

    Implementing ArticlesDialogBox class

---
+++ src/main/java/design_patterns/behavioral/mediator/ArticlesDialogBox.java
@@ -0,0 +1,31 @@
+package design_patterns.behavioral.mediator;
+
+public class ArticlesDialogBox extends DialogBox {
+
+    private ListBox articlesListBox = new ListBox(this);
+    private TextBox titleTextBox = new TextBox(this);
+    private Button saveButton = new Button(this);
+
+    @Override
+    public void changed(UIControl control) {
+        if (control == articlesListBox) articleSelected();
+        else if (control == titleTextBox) titleChanged();
+    }
+
+    private void articleSelected() {
+        titleTextBox.setContent(articlesListBox.getSelection());
+        saveButton.setEnabled(true);
+    }
+
+    private void titleChanged() {
+        String content = titleTextBox.getContent();
+        boolean isEmpty = content == null || content.isEmpty();
+        saveButton.setEnabled(!isEmpty);
+    }
+
+    public void simulateUserInteraction() {
+        articlesListBox.setSelection("Article 1");
+        System.out.println("TextBox: " + titleTextBox.getContent());
+        System.out.println("Button: " + saveButton.isEnabled());
+    }
+}
