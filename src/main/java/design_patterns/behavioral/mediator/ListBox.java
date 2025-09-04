337ba81d40e032a1b3f07e7ebd295e04db5f53f6
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-04 13:08:52 +0330

    Connecting ListBox to dialogBox

--- src/main/java/design_patterns/behavioral/mediator/ListBox.java
+++ src/main/java/design_patterns/behavioral/mediator/ListBox.java
@@ -3,11 +3,16 @@ package design_patterns.behavioral.mediator;
 public class ListBox extends UIControl {
     private String selection;
 
+    public ListBox(DialogBox owner) {
+        super(owner);
+    }
+
     public String getSelection() {
         return selection;
     }
 
     public void setSelection(String selection) {
         this.selection = selection;
+        owner.changed(this);
     }
 }
