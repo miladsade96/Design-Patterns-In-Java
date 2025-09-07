9d483565d260c8560c21dfc6accbf015b7cde36e
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-07 19:12:16 +0330

    Add username and password fields to HttpRequest class

--- src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/HttpRequest.java
+++ src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/HttpRequest.java
@@ -1,4 +1,11 @@
 package design_patterns.behavioral.chain_of_responsibility_pattern;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class HttpRequest {
+    private String username;
+    private String password;
 }
