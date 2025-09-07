159e78d62785803f59f12d20281696a107f76d35
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-07 19:12:26 +0330

    Update WebServer to handle HttpRequest with authentication, logging, and compression

--- src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/WebServer.java
+++ src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/WebServer.java
@@ -1,6 +1,9 @@
 package design_patterns.behavioral.chain_of_responsibility_pattern;
 
 public class WebServer {
-    public void handle() {
+    public void handle(HttpRequest request) {
+        // Authentication
+        // Logging
+        // Compression
     }
 }
