5879ed6e2a7006f65d5eab65a6228f54347c47df
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-07 19:39:28 +0330

    Add Handler to WebServer for processing HttpRequest through Chain of Responsibility

--- src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/WebServer.java
+++ src/main/java/design_patterns/behavioral/chain_of_responsibility_pattern/WebServer.java
@@ -1,9 +1,16 @@
 package design_patterns.behavioral.chain_of_responsibility_pattern;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class WebServer {
+
+    private Handler handler;
+
     public void handle(HttpRequest request) {
         // Authentication
         // Logging
         // Compression
+        handler.handle(request);
     }
 }
