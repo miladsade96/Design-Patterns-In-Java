90b4600cca55b6eb135480ccee0ae7a993f1c470
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-10 18:20:20 +0330

    Creating HtmlDocument class

---
+++ src/main/java/design_patterns/behavioral/visitor_pattern/HtmlDocument.java
@@ -0,0 +1,12 @@
+package design_patterns.behavioral.visitor_pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlDocument {
+    private final List<HtmlNode> nodes = new ArrayList<>();
+
+    public void addNode(HtmlNode node) {
+        nodes.add(node);
+    }
+}
