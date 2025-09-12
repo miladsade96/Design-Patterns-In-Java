16fe58f0f52493fba09cde6edb37c7f04e1347d3
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-12 14:10:07 +0330

    Implement singleton pattern in ConfigManager using Lombok

--- src/main/java/design_patterns/creational/singleton_pattern/ConfigManager.java
+++ src/main/java/design_patterns/creational/singleton_pattern/ConfigManager.java
@@ -1,10 +1,16 @@
 package design_patterns.creational.singleton_pattern;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class ConfigManager {
 
+    @Getter
+    private static final ConfigManager instance = new ConfigManager();
     private final Map<String, Object> settings = new HashMap<>();
 
     public void set(String key, Object value) {
