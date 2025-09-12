9cc8e8bb0057d849d65366eaa53a25673a3d8d3a
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-12 13:58:31 +0330

    Add ConfigManager class to manage application settings

---
+++ src/main/java/design_patterns/creational/singleton_pattern/ConfigManager.java
@@ -0,0 +1,17 @@
+package design_patterns.creational.singleton_pattern;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigManager {
+
+    private final Map<String, Object> settings = new HashMap<>();
+
+    public void set(String key, Object value) {
+        settings.put(key, value);
+    }
+
+    public Object get(String key) {
+        return settings.get(key);
+    }
+}
