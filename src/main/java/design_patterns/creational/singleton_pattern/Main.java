f482866c1144930664b73e7bf80bb204def386c4
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-12 14:10:22 +0330

    Refactor Main class to use singleton instance of ConfigManager

--- src/main/java/design_patterns/creational/singleton_pattern/Main.java
+++ src/main/java/design_patterns/creational/singleton_pattern/Main.java
@@ -2,11 +2,11 @@ package design_patterns.creational.singleton_pattern;
 
 public class Main {
     public static void main(String[] args) {
-        ConfigManager config = new ConfigManager();
+        ConfigManager config = ConfigManager.getInstance();
         config.set("app_name", "MyApplication");
         System.out.println("App Name: " + config.get("app_name"));
 
-        ConfigManager anotherConfig = new ConfigManager();
-        System.out.println("App Name from another instance: " + anotherConfig.get("app_name")); // This will print null since it's a different instance
+        ConfigManager anotherConfig = ConfigManager.getInstance();
+        System.out.println("App Name from another reference: " + anotherConfig.get("app_name"));
     }
 }
