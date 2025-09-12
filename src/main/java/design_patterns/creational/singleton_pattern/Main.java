79fa0ecb9f50676145e90b1a4591e03d7dca084f
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-12 14:02:47 +0330

    Demonstrate ConfigManager usage in Main class

--- src/main/java/design_patterns/creational/singleton_pattern/Main.java
+++ src/main/java/design_patterns/creational/singleton_pattern/Main.java
@@ -2,6 +2,11 @@ package design_patterns.creational.singleton_pattern;
 
 public class Main {
     public static void main(String[] args) {
-        
+        ConfigManager config = new ConfigManager();
+        config.set("app_name", "MyApplication");
+        System.out.println("App Name: " + config.get("app_name"));
+
+        ConfigManager anotherConfig = new ConfigManager();
+        System.out.println("App Name from another instance: " + anotherConfig.get("app_name")); // This will print null since it's a different instance
     }
 }
