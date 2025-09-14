4a3586ec1df84467263e350006e1701f13a71b25
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-14 19:05:01 +0330

    Implementing render method

--- src/main/java/design_patterns/creational/factory_method_pattern/MatchaViewEngine.java
+++ src/main/java/design_patterns/creational/factory_method_pattern/MatchaViewEngine.java
@@ -1,4 +1,9 @@
 package design_patterns.creational.factory_method_pattern;
 
+import java.util.Map;
+
 public class MatchaViewEngine {
+    public String render(String viewName, Map<String, Object> context) {
+        return "View rendered by Matcha!";
+    }
 }
