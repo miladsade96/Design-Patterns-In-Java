4ae87038fbd671314765aaaea4c6369f8729acfe
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-15 19:14:49 +0330

    Implementing render method of controller class

--- src/main/java/design_patterns/creational/factory_method_pattern/Controller.java
+++ src/main/java/design_patterns/creational/factory_method_pattern/Controller.java
@@ -1,4 +1,14 @@
 package design_patterns.creational.factory_method_pattern;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
 public class Controller {
+    public void render(String viewName, Map<String, Object> context) {
+        MatchaViewEngine viewEngine = new MatchaViewEngine();
+        String html = viewEngine.render(viewName, context);
+        log.info(html);
+    }
 }
