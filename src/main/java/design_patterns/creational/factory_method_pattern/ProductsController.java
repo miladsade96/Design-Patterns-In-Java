e993a00aea0b502597b01aa0d543c650ec229ba1
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-16 18:56:26 +0330

    Implementing listProducts method

--- src/main/java/design_patterns/creational/factory_method_pattern/ProductsController.java
+++ src/main/java/design_patterns/creational/factory_method_pattern/ProductsController.java
@@ -1,4 +1,11 @@
 package design_patterns.creational.factory_method_pattern;
 
-public class ProductsController {
+import design_patterns.creational.factory_method_pattern.matcha.Controller;
+
+import java.util.Map;
+
+public class ProductsController extends Controller {
+    public void listProducts() {
+        render("products.html", Map.of("products", new String[]{"Apple", "Banana", "Orange"}));
+    }
 }
