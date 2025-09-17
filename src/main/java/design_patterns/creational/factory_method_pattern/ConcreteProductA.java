ee91cffba06b3698829ee370e46e0e5c977c148e
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-17 18:44:47 +0330

    Implementing ConcreteProductA class

---
+++ src/main/java/design_patterns/creational/factory_method_pattern/ConcreteProductA.java
@@ -0,0 +1,11 @@
+package design_patterns.creational.factory_method_pattern;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteProductA implements Product {
+    @Override
+    public void doSomething() {
+        log.info("ConcreteProductA doSomething");
+    }
+}
