53a9619557ae7f1e034ac784ee701ac40c3fd628
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-17 18:48:07 +0330

    Implementing Creator class

---
+++ src/main/java/design_patterns/creational/factory_method_pattern/Creator.java
@@ -0,0 +1,10 @@
+package design_patterns.creational.factory_method_pattern;
+
+public abstract class Creator {
+    public abstract Product factoryMethod();
+
+    public void doWork() {
+        Product product = factoryMethod();
+        product.doSomething();
+    }
+}
