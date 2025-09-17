d47cbae2f81457e23a2a6dc9e809dcbbb7779085
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-17 18:49:19 +0330

    Implementing ConcreteCreatorA class

---
+++ src/main/java/design_patterns/creational/factory_method_pattern/ConcreteCreatorA.java
@@ -0,0 +1,8 @@
+package design_patterns.creational.factory_method_pattern;
+
+public class ConcreteCreatorA extends Creator {
+    @Override
+    public Product factoryMethod() {
+        return new ConcreteProductA();  // Create and return an instance of ConcreteProductA
+    }
+}
