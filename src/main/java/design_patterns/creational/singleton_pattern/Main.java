58ef2dea89ef26dd906b1e11f9c67aa2124dd7fe
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-12-31 16:55:03 +0330

    Add example usage for LazySingleton in Main.java

--- src/main/java/design_patterns/creational/singleton_pattern/Main.java
+++ src/main/java/design_patterns/creational/singleton_pattern/Main.java
@@ -15,5 +15,13 @@ public class Main {
         EagerSingleton eagerSingleton2 = EagerSingleton.getInstance();
         System.out.println("eagerSingleton1: " + eagerSingleton1.hashCode());
         System.out.println("eagerSingleton2: " + eagerSingleton2.hashCode());
+        System.out.println("Eager singletons are the same instance: " + (eagerSingleton1 == eagerSingleton2));
+
+        // Lazy Initialization:
+        LazySingleton lazySingleton1 = LazySingleton.getInstance();
+        LazySingleton lazySingleton2 = LazySingleton.getInstance();
+        System.out.println("lazySingleton1: " + lazySingleton1.hashCode());
+        System.out.println("lazySingleton2: " + lazySingleton2.hashCode());
+        System.out.println("Lazy singletons are the same instance: " + (lazySingleton1 == lazySingleton2));
     }
 }
